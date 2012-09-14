86762537eb54761fe196527bc858b5b289b9e392
George Menhal III <gmenhal@gmail.com>
2012-09-14 09:56:47 -0500

    Example: Get random value from array
    Shows how to use the srand() and rand() methods to retrieve a random
    value from a declared array.

--- ChatbotPrototype/ChatbotPrototype/Main.cpp
+++ ChatbotPrototype/ChatbotPrototype/Main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
 
 namespace ChatBot {
 	#include <string>
 	using namespace std;
 
-	string outputCB = "Hello World";
+	string outputCB;
+	string greetingsArrayCB[4] = {"WHERE WERE THE OTHER DRUGS GOING?!", "SWEAR TO ME!", "DO I LOOK LIKE A COP?!", "WHERE ARE THEY?!"};
 	
 	// Displays the std::string value of ChatBot variable 'outputCB'
 	void showOutputCB(){
@@ -16,8 +19,13 @@ namespace ChatBot {
 using namespace std;
 
 void main(){
+	srand((int)time(NULL));
+	ChatBot::outputCB = ChatBot::greetingsArrayCB[rand() % 5];
+
+	cout<< endl<< endl;
 
 	ChatBot::showOutputCB();
 	system ("pause");
+	main();
 
 }
